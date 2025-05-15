f6df4bd300fecc8ee9d1326100caadd6012b986d
Nathan Jackson <ntj8845@g.rit.edu>
2025-05-15 04:10:42 -0400

    forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,76 @@ import { Link } from 'react-router-dom';
 function Contact() {
   return (
     <div>
-      <h1>Contact</h1>
-      
+      <h1 className='BigHeader '> WANT TO BECOME A <br/> PARTNERING <br/> BUSINESS? </h1>
+      <p className='MidText text-left'> At GroupSoup, we believe community is a team effort. We need the partnership of Rochester’s small businesses to turn our vision of a vibrant, connected community into reality. Tell us about your goals, and let’s see how we can help! </p>
+    
+      <form>
+        <div className='d-flex align-items-center justify-content-center'>
+          <label className='profileHeader m-5 mb-2 mt-2 w-25'>
+            Company name:
+          </label>
+          <input type='text' id='companyNameInput' className='inputBox m-2'>
+          
+          </input>
+        </div>
+
+        <div className='d-flex align-items-center justify-content-center'>
+          <label className='profileHeader m-5 mt-2 mb-2 w-25'>
+            Email:
+          </label>
+          <input type='email' id='companyEmailInput' className='inputBox m-2'>
+
+          </input>
+        </div>
+
+        <div className='d-flex align-items-center justify-content-center'>
+          <label className='profileHeader m-5 mt-2 mb-2 w-25'>
+            Phone:
+          </label>
+          <input type='email' id='companyPhoneInput' className='inputBox m-2'>
+
+          </input>
+        </div>
+
+        <div className='d-flex align-items-center justify-content-center'>
+          <label className='profileHeader m-5 mt-2 mb-2 w-25'>
+            Address:
+          </label>
+          <input type='email' id='companyAddressinput' className='inputBox m-2'>
+
+          </input>
+        </div>
+
+        <div className='d-flex align-items-center justify-content-center'>
+          <label className='profileHeader m-5 mt-2 mb-2 w-25'>
+            Subject:
+          </label>
+          <input type='email' id='companySubjectInput' className='inputBox m-2'>
+
+          </input>
+        </div>
+
+        <div className='d-flex align-items-center justify-content-center'>
+          <label className='profileHeader m-5 mt-2 mb-2 w-25'>
+            Message:
+          </label>
+          <textarea rows="4" id='companyMessageInput' className='m-2'>
+
+          </textarea>
+        </div>
+
+        <div className='d-flex align-items-center justify-content-center'>
+          <label className='profileHeader m-5 mt-2 mb-2 w-25'>
+            How did you hear about us?
+          </label>
+          <textarea rows="6" id='companyHearInput' className='m-2' >
+
+          </textarea>
+        </div>
+
+        <button type='submit' className='submitButton d-flex align-items-center justify-content-center'> SUBMIT </button>
+      </form>
+
     </div>
   );
 }
