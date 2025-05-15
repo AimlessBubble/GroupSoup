e95328446f3033da24290d056d0ae7e0e3d6c8fc
Nathan Jackson <ntj8845@g.rit.edu>
2025-05-15 03:16:50 -0400

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,8 +5,30 @@ import { Link } from 'react-router-dom';
 function Register() {
   return (
     <div>
-      <h1>Register</h1>
-      
+      <h1 className='BigHeader text-center'>JOIN THE COMMUNITY <br/> REVOLUTION</h1>
+      <p className='MidText secondThird'> Be among the first to discover what life can be like with an accessible, vibrant community through GroupSoup in Rochester, NY! Please provide the information below so that we can reach out to you when we launch to the public!</p>
+
+      <form>
+        <div className='d-flex align-items-center justify-content-center'>
+          <label className='profileHeader m-5'>
+            Name:
+          </label>
+          <input type='text' id='nameInput' className='inputBox'>
+          
+          </input>
+        </div>
+
+        <div className='d-flex align-items-center justify-content-center'>
+          <label className='profileHeader m-5 mt-1 mb-1'>
+            Email:
+          </label>
+          <input type='email' id='emailInput' className='inputBox'>
+
+          </input>
+        </div>
+
+        <button type='submit' className='submitButton'> SUBMIT </button>
+      </form>
     </div>
   );
 }
