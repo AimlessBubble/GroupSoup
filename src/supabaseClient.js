1623a17a2e636f540228c4ddd7536ee449b18582
Nathan Jackson <ntj8845@g.rit.edu>
2025-06-09 18:31:43 -0400

    environment testing

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,5 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
+console.log("SUPABASE_URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("SUPABASE_KEY:", process.env.REACT_APP_SUPABASE_KEY);
 
 export default supabase;
